8bf5d2448fceae664dfbfd5a34fe885b1023370f
Wade Doolan <wadedoolan@hotmail.com>
2023-03-05 15:11:50 +1000

    Add remote url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import App from './App';
 
 const local_host = "http://localhost:5000";
 
-axios.defaults.baseURL = local_host;
+const remote_host = "https://customer-credit-app-api-production-d09b.up.railway.app"
+
+axios.defaults.baseURL = remote_host;
 
 axios.interceptors.request.use((config) => {
   const token = localStorage.getItem("token")
